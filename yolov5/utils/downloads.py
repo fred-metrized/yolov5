c5e2cd5ca1488f9579d59166ae0b5a61437b068a
fred-metrized <fredz@metrized.com>
2024-10-21 12:11:18 -0700

    bump huggingface_hub

--- yolov5/utils/downloads.py
+++ yolov5/utils/downloads.py
@@ -147,8 +147,8 @@ def get_model_filename_from_hfhub(repo_id, hf_token=None):
 
 def attempt_download_from_hub(repo_id, hf_token=None, revision=None):
     from huggingface_hub import hf_hub_download, list_repo_files
-    from huggingface_hub.utils._errors import RepositoryNotFoundError
-    from huggingface_hub.utils._validators import HFValidationError
+    from huggingface_hub.utils import RepositoryNotFoundError
+    from huggingface_hub.utils import HFValidationError
 
     try:
         repo_files = list_repo_files(repo_id=repo_id, repo_type='model', token=hf_token)
